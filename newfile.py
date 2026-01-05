741661aab058b8e614d67f633663224b8201861b
Saurabh <ysaurabhyadav35@gmail.com>
2026-01-05 17:42:39 +0530

    Updated code in newfile.py2.2

--- newfile.py
+++ newfile.py
@@ -4,4 +4,14 @@ b = 10
 print(a + b)
 print("Sum of a and b is:", a + b)
 def greet(name):
-    return "Hello, " + name + "!"                       
+    return "Hello, " + name + "!"    
+print(greet("Alice"))
+for i in range(3):
+    print("Iteration:", i)                                  
+if a < b:
+    print("a is less than b")       
+else:
+    print("a is not less than b")       
+while a < b:
+    print("a is still less than b:", a)
+    a += 1                   
